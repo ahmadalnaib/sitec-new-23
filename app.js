c6749f77fb107194a8db78d7cd06ffeef2dc4b82
ahmad.al-naib <ahmad.al-naib@sitec.de>
2022-10-13 12:31:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// external js: isotope.pkgd.js
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    filter: '.av70',
+    layoutMode: 'fitRows',
+    
+  });
+  
+  // filter functions
+  var filterFns = {
+
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+  
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  // change is-checked class on buttons
+$('.btn-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+  
+
+    
+  
